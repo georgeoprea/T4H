c7577ca4f8707dc2a3981df1fd2bb629e591d557
George Oprea <georgeoprea10@gmail.com>
2021-11-08 02:02:19 +0200

    Add partial server health check

--- React/t4h-react-app/src/App.js
+++ React/t4h-react-app/src/App.js
@@ -1,11 +1,30 @@
 import './App.css';
+import React from 'react';
 import About from './components/About';
 import Nav from './components/Nav';
 import Users from './components/Users';
 import Login from './components/Login';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import $ from 'jquery';
+
+function isServerHealthy() {
+  var context = this;
+  $.ajax({
+    url: 'http://localhost:5000/healthcheck',
+    method: 'GET',
+    success: function(response) {
+      console.log('server is healthy');
+      // App.setHealth(true);
+    },
+    error: function(error){
+      console.log("server is sick");
+    }
+  })
+}
 
 function App() {
+  const [health, setHealth] = React.useState(false);
+  isServerHealthy();
   return (
     <Router>
       <div className="App">
